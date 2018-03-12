3c6829ed1438a31fc4bef35cb8ee9eaf57c06445
addisonday <day.addison@yahoo.com>
2018-03-12 16:34:50 -0700

    Removed unecessary -e flag from echo commands

--- babel-boilerplate-new.sh
+++ babel-boilerplate-new.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# Script to setup up Babel
+# Script to setup Babel
 # Written by, Addison Coquillette-Day
 
 # Define replacement strings in package.json
 RE='"[a-z]*":\s"[a-z]*\s."\w*:\s\w*\s\w*\s\w*."\s..\s\w*\s[0-9]"'
 BUILD="\"build\": \"babel src -d lib\""
 
-echo -e "[-- Setting up Babel --]"
-echo -e "[-- Creating src directory... --]"
+echo "[-- Setting up Babel --]"
+echo "[-- Creating src directory... --]"
 mkdir -p babel-setup/src
 cd babel-setup/
 echo
-echo -e "[-- Creating README... --]"
+echo "[-- Creating README... --]"
 touch README.md
-echo -e "[-- Configuring project... --]"
+echo "[-- Configuring project... --]"
 npm init -y
 echo
 echo "[-- Setting build command in package.json --]"
 sed -i "s/$RE/$BUILD/g" package.json
-echo -e "[-- Installing Babel CLI and env preset --]"
+echo "[-- Installing Babel CLI and env preset --]"
 echo
 npm install --save-dev babel-cli babel-preset-env
 echo
@@ -26,4 +26,4 @@ touch .babelrc
 echo "{
   \"presets\": [\"env\"]
 }" > .babelrc
-echo -e "[-- Done... --]"
+echo "[-- Done... --]"
